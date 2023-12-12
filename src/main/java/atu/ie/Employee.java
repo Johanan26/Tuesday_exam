256509817fa71ec7592d71dc6c7c81944fed3e9c
JOHANAN SEERUTHEN - STUDENT <g00419446@atu.ie>
2023-12-12 12:59:47 +0000

    My Unfinished Code

---
+++ src/main/java/atu/ie/Employee.java
@@ -0,0 +1,52 @@
+package atu.ie;
+
+import java.util.Scanner;
+import java.lang.String;
+public class Employee {
+
+    public static void main(String[] args) {
+
+        String Name;
+        int pps;
+        String employmentType;
+        String gender;
+        int age;
+
+public void getEmployeeInput() {
+            System.out.println("Enter Name:");
+            this.Name = Scanner.nextLine();
+            System.out.println("Enter pps:");
+            this.pps = Scanner.nextLine();
+            System.out.println("Enter Employment Type:");
+            this.employmentType = Scanner.nextLine();
+            System.out.println("Enter Gender:");
+            this.gender = Scanner.nextLine();
+            System.out.println("Enter age:");
+            this.age = Scanner.nextLine();
+        }
+
+       while(true){
+
+            try{
+    if(age < 16 || age > 66 && Name< value.lenght(3) || Name > value.lenght(25) && pps != value.lenght(11) && gender != man || women|| non binary){
+        throw new IllegalArgumentException("Invalid Age: Age must be over 16 and less than 66.");
+    }
+                get
+    System.out.println("Enter age:");
+    if(myage < 16 || myage > 66) {
+        System.out.println("you Enterd" + myage);
+        break;
+    }
+    else {
+        System.out.println("not valid number");
+    }
+                }
+            catch (IllegalArgumentException e){
+                System.out.println("Error:" + e.getMessage());
+            }
+            }
+
+        }
+
+    }
+}
---
+++ src/main/java/atu/ie/EmployeeApp.java
@@ -0,0 +1,32 @@
+package atu.ie;
+
+import javax.lang.model.element.Name;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        user.getEmployeeInput();
+        user.displayinfo();
+        Name = new Name();
+        pps = new pps();
+        employmentType = new employmentType();
+        gender = new gander();
+        age = new age();
+    }
+         public EmployeeApp() {
+            Name = Johanan;
+            pps = 12345678912;
+            employmentType = Full Time;
+            gender = Man;
+            age = 18;
+        }
+
+    @Override
+    public String toString() {
+        return "EmployeeApp{" +
+                "get='" + get + '\'' +
+                '}';
+    }
+
+    public String get
+    }
+}
